38e480f1f57c1ef459579efa612385a9d56a85cd
Akshay Bhandary <akshayb@yahoo-inc.com>
2017-01-18 12:05:09 -0800

    updates to add in the optional features

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -34,11 +34,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var thirdDivisionAmountLabel: UILabel!
     
     let kDefaultPercentageStringKey = "kDefaultPercentageStringKey"
+    let kBillAmountKey              = "kBillAmountKey"
+    let kCurrentEpochTimeKey        = "kCurrentEpochTimeKey"
     
     var topViewOffset = true;
     
     var tipPercentageArray = [0.18, 0.20, 0.25];
     var divisionArray      = [0.5, 0.33, 0.25];
+    var billAmount         = 0.0;
+    
     override func viewDidLoad() {
     
         super.viewDidLoad()
@@ -48,6 +52,12 @@ class ViewController: UIViewController {
         self.containerViewVeriticalSpaceToBottomConstraint.constant = -150;
         
         setupAmountTextField();
+        
+        
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(willResignActive),
+                                               name: Notification.Name.UIApplicationWillResignActive, object: nil);
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -55,6 +65,13 @@ class ViewController: UIViewController {
         let defaultPercentage = defaults.integer(forKey: kDefaultPercentageStringKey);
         
         self.tipSegmentedControl.selectedSegmentIndex = defaultPercentage;
+    }
+    
+    func willResignActive() {
+        let defaults = UserDefaults()
+        
+        defaults.set(self.billAmount, forKey: kBillAmountKey)
+        defaults.set(Date().timeIntervalSince1970, forKey: kCurrentEpochTimeKey)
     }
     
     func setupAmountTextField() {
@@ -72,6 +89,16 @@ class ViewController: UIViewController {
         borderLayer.borderWidth = borderWidth;
         self.amountTextField.layer.addSublayer(borderLayer);
         self.amountTextField.layer.masksToBounds = true;
+        
+        // setup default amount
+        let defaults = UserDefaults()
+        let lastEpoch = defaults.double(forKey: kCurrentEpochTimeKey) as TimeInterval;
+        
+        print(Date().timeIntervalSince(Date(timeIntervalSince1970: lastEpoch)));
+        if Date().timeIntervalSince(Date(timeIntervalSince1970: lastEpoch)) < 60 {
+            self.amountTextField.text =  "\(defaults.double(forKey: kBillAmountKey))";
+            handleBillAmountChange()
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -83,8 +110,13 @@ class ViewController: UIViewController {
     }
 
     @IBAction func amountTextFieldDidChange(_ sender: Any) {
-
+        handleBillAmountChange()
+    }
+    
+    func handleBillAmountChange() {
         
+        // A bill amount has been entered, show the tip amount and total amount and
+        // divide by people views. Move the bill amount view to the top.
         if topViewOffset {
             topViewOffset = false;
             self.bottomView.isHidden = false;
@@ -95,10 +127,10 @@ class ViewController: UIViewController {
             UIView.animate(withDuration: 0.5, animations: {
                 self.view.layoutIfNeeded()
             });
-            
-            
         }
         
+        // If a bill amount has not been entered, then offset the top view and hide views like
+        // tip amount, total etc which will all be zeroes anyways.
         if self.amountTextField.text!.characters.count == 0 {
             topViewOffset = true;
             self.bottomView.isHidden = true;
@@ -110,21 +142,42 @@ class ViewController: UIViewController {
             });
         }
         
+        // calculate tip amount and total amount
         let amount = Double(self.amountTextField.text!) ?? 0;
+        self.billAmount = amount;
         let tipAmount = amount * self.tipPercentageArray[self.tipSegmentedControl.selectedSegmentIndex]
         let totalAmount = amount + tipAmount;
         
-        self.tipAmountLabel.text = String(format: "$%.2f", tipAmount);
-        self.totalAmountLabel.text = String(format: "$%.2f", totalAmount);
+        // setup number formatter for localized currency
+        let formatter = NumberFormatter()
+        formatter.numberStyle = NumberFormatter.Style.currency;
+        formatter.locale = Locale.current;
         
-        self.firstDivisionAmountLabel.text = String(format: "$%.2f", totalAmount * divisionArray[0]);
-        self.secondDivisionAmountLabel.text = String(format: "$%.2f", totalAmount * divisionArray[1]);
-        self.thirdDivisionAmountLabel.text = String(format: "$%.2f", totalAmount * divisionArray[2]);
+        // set localized tip amount
+        var localizedMoneyString = formatter.string(from: NSNumber(value: tipAmount));
+        self.tipAmountLabel.text = localizedMoneyString
+        
+        // set localized total amount
+        localizedMoneyString = formatter.string(from: NSNumber(value: totalAmount));
+        self.totalAmountLabel.text = localizedMoneyString;
+        
+        // set localized first division amount
+        localizedMoneyString = formatter.string(from: NSNumber(value: totalAmount * divisionArray[0]));
+        self.firstDivisionAmountLabel.text = localizedMoneyString
+        
+        // set localized second division amount
+        localizedMoneyString = formatter.string(from: NSNumber(value: totalAmount * divisionArray[1]));
+        self.secondDivisionAmountLabel.text = localizedMoneyString
+        
+        // set localized third division amount
+        localizedMoneyString = formatter.string(from: NSNumber(value: totalAmount * divisionArray[2]));
+        self.thirdDivisionAmountLabel.text = localizedMoneyString;
     }
 
     @IBAction func topViewTapped(_ sender: UITapGestureRecognizer) {
         self.amountTextField.resignFirstResponder()
     }
+    
     
 }
 
